baec3676dbfa8f55a8fcbf0ebdc5d5ccd805c78e
ramzanBilal <ramzanbilal14@gmail.com>
2025-09-30 11:16:32 +0500

    remove duplicate import

--- src/components/HapticTab.tsx
+++ src/components/HapticTab.tsx
@@ -1,7 +1,6 @@
 import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
 import { PlatformPressable } from "@react-navigation/elements";
 import * as Haptics from "expo-haptics";
-import * as Haptics from "expo-haptics";
 
 export function HapticTab(props: BottomTabBarButtonProps) {
   return (
